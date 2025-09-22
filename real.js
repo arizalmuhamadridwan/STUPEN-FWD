c21d8e8bc06b0f479c5e2e9675069541739db219
arizalmuhamadridwan <146161249+arizalmuhamadridwan@users.noreply.github.com>
2025-09-22 14:17:24 +0700

    Update real.js

--- real.js
+++ real.js
@@ -1,9 +1,7 @@
-// Data awal
 let namaProduk = ["Laptop","Mouse","Keyboard"];
 let harga = [7000000,200000,350000];
 let stok = [5,10,7];
 
-// Konversi data awal ke array objek
 let produk = [];
 for (let i = 0; i < namaProduk.length; i++) {
   produk.push({
@@ -14,25 +12,19 @@ for (let i = 0; i < namaProduk.length; i++) {
   });
 }
 
-// Fungsi tambah produk
 function tambah() {  
   let nama = document.getElementById("namaProduk").value;
   let hargaProduk = parseInt(document.getElementById("harga").value);
   let stokProduk = parseInt(document.getElementById("stok").value);
-
   let id = produk.length > 0 ? produk[produk.length - 1].id + 1 : 1;
   produk.push({ id, namaProduk: nama, harga: hargaProduk, stok: stokProduk });
-
   let output = `Produk "${nama}" berhasil ditambahkan!`;
   document.getElementById("informasi").innerHTML = output;
-
-  // reset input
   document.getElementById("namaProduk").value = "";
   document.getElementById("harga").value = "";
   document.getElementById("stok").value = "";
 }
 
-// Fungsi tampilkan produk
 function tampilkan() {
   let output = "======== Daftar Produk ========<br>";
   if (produk.length === 0) {
